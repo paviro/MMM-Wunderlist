5bf0688439179045635994fe3133748f73408c84
Marcin Bielecki <marcin.bielecki@eracent.com>
2017-03-23 09:02:16 +0100

    use wunderlist node.js api instead of requesting http api

--- MMM-Wunderlist.js
+++ MMM-Wunderlist.js
@@ -1,3 +1,4 @@
+"use strict"
 /* global Module */
 
 /* Magic Mirror
@@ -9,81 +10,81 @@
 
 Module.register("MMM-Wunderlist", {
 
- defaults: {
-	maximumEntries: 10,
-	order: "normal",
-	lists: ["inbox"],
-	interval: 60,
-	fade: true,
-	fadePoint: 0.25
- },
+  defaults: {
+    maximumEntries: 10,
+    order: "normal",
+    lists: ["inbox"],
+    interval: 60,
+    fade: true,
+    fadePoint: 0.25
+  },
 
- // Override socket notification handler.
- socketNotificationReceived: function(notification, payload) {
-	if (notification === "TASKS") {
-	 this.tasks = payload
-	 this.updateDom(3000);
-	} else if (notification === "STARTED") {
-	 console.log(notification);
-	 this.sendSocketNotification("addLists", this.config.lists);
-	}
- },
+  // Override socket notification handler.
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "TASKS") {
+      this.tasks = payload
+      this.updateDom(3000);
+    } else if (notification === "STARTED") {
+      console.log(notification);
+      this.sendSocketNotification("addLists", this.config.lists);
+    }
+  },
 
- start: function() {
-	this.tasks = [];
-	this.sendSocketNotification("CONFIG", this.config);
-	this.sendSocketNotification("CONNECTED");
-	Log.info("Starting module: " + this.name);
- },
+  start: function () {
+    this.tasks = [];
+    this.sendSocketNotification("CONFIG", this.config);
+    this.sendSocketNotification("CONNECTED");
+    Log.info("Starting module: " + this.name);
+  },
 
- getTodos: function() {
-	var tasksShown = [];
+  getTodos: function () {
+    var tasksShown = [];
 
-	for (var i = 0; i < this.config.lists.length; i++) {
-	 if (typeof this.tasks[this.config.lists[i]] != "undefined") {
-		var list = this.tasks[this.config.lists[i]];
+    for (var i = 0; i < this.config.lists.length; i++) {
+      if (typeof this.tasks[this.config.lists[i]] != "undefined") {
+        var list = this.tasks[this.config.lists[i]];
 
-		for (var todo in list) {
-                 if(this.config.order == "reversed"){
-                   tasksShown.push(list[todo]);
-                 } else {
-                   tasksShown.unshift(list[todo]);
-                 }
-		}
-	 }
-	}
-	return tasksShown.slice(0, this.config.maximumEntries);
+        for (var todo in list) {
+          if (this.config.order == "reversed") {
+            tasksShown.push(list[todo]);
+          } else {
+            tasksShown.unshift(list[todo]);
+          }
+        }
+      }
+    }
+    return tasksShown.slice(0, this.config.maximumEntries);
 
- },
- getDom: function() {
-	var wrapper = document.createElement("table");
-	wrapper.className = "normal small light";
+  },
+  getDom: function () {
+    var wrapper = document.createElement("table");
+    wrapper.className = "normal small light";
 
-	var todos = this.getTodos();
+    var todos = this.getTodos();
 
 
-	for (var i = 0; i < todos.length; i++) {
-	 var titleWrapper = document.createElement("tr");
-	 titleWrapper.innerHTML = todos[i];
-	 titleWrapper.className = "title bright";
-	 wrapper.appendChild(titleWrapper);
+    for (var i = 0; i < todos.length; i++) {
+      var titleWrapper = document.createElement("tr");
+      titleWrapper.innerHTML = todos[i];
+      titleWrapper.className = "title bright";
+      wrapper.appendChild(titleWrapper);
 
-	 // Create fade effect by MichMich (MIT)
-	 if (this.config.fade && this.config.fadePoint < 1) {
-		if (this.config.fadePoint < 0) {
-		 this.config.fadePoint = 0;
-		}
-		var startingPoint = todos.length * this.config.fadePoint;
-		var steps = todos.length - startingPoint;
-		if (i >= startingPoint) {
-		 var currentStep = i - startingPoint;
-		 titleWrapper.style.opacity = 1 - (1 / steps * currentStep);
-		}
-	 }
-	 // End Create fade effect by MichMich (MIT)
-	}
+      // Create fade effect by MichMich (MIT)
+      if (this.config.fade && this.config.fadePoint < 1) {
+        if (this.config.fadePoint < 0) {
+          this.config.fadePoint = 0;
+        }
+        var startingPoint = todos.length * this.config.fadePoint;
+        var steps = todos.length - startingPoint;
+        if (i >= startingPoint) {
+          var currentStep = i - startingPoint;
+          titleWrapper.style.opacity = 1 - (1 / steps * currentStep);
+        }
+      }
+      // End Create fade effect by MichMich (MIT)
+    }
 
-	return wrapper;
- }
+    return wrapper;
+  }
 
 });
