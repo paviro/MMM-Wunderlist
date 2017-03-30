d2398a546c28b54a1009a09df0ef9e1339a45fe3
Marcin Bielecki <marcin.bielecki@eracent.com>
2017-03-30 13:00:06 +0200

    first step - replace default wrapper with string formatter

---
+++ String.format.js
@@ -0,0 +1,11 @@
+﻿"use strict"
+
+// add string format method
+if (!String.prototype.format) {
+  String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+      return typeof args[number] != 'undefined' ? args[number] : match;
+    });
+  };
+}
